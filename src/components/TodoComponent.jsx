d6d545ec30bb78b9d7bd40d7f759ed0ad1a3d9ee
Mekontso Anderson Saa <mekontsoanderson@gmail.com>
2024-05-02 18:14:00 +0100

    Todo component

--- src/components/TodoComponent.jsx
+++ src/components/TodoComponent.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { useState } from 'react'
 
 const TodoComponent = () => {
+
+    const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
+    const [completed, setCompleted] = useState("")
+
     return (
-        <div>TodoComponent</div>
+        <div className='container'>
+            <div className='row'>
+                <div className='card col-md-6 offset-md-3 offset-md-3'>
+                    <h2 className='text-center'>Add Todo</h2>
+                    <div className='card-body'>
+                        <form>
+                            <div className='form-group mb-2'>
+                                <label className='form-label'>Todo title</label>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
