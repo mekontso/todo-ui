ad74c0287fbd23606de9269059ccad22a5fd8347
mekontso <mekontsoanderson@gmail.com>
2024-05-06 21:41:20 +0100

    Add todo

--- src/components/TodoComponent.jsx
+++ src/components/TodoComponent.jsx
@@ -1,28 +1,81 @@
-import React from 'react'
-import { useState } from 'react'
+import React from "react";
+import { useState } from "react";
+import { saveTodo } from "../services/TodoService";
+import { useNavigate } from "react-router-dom";
 
 const TodoComponent = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [completed, setCompleted] = useState(false);
+  const navigate = useNavigate();
 
-    const [title, setTitle] = useState("")
-    const [description, setDescription] = useState("")
-    const [completed, setCompleted] = useState("")
+  const saveOrUpdateTodo = (e) => {
+    e.preventDefault();
 
-    return (
-        <div className='container'>
-            <div className='row'>
-                <div className='card col-md-6 offset-md-3 offset-md-3'>
-                    <h2 className='text-center'>Add Todo</h2>
-                    <div className='card-body'>
-                        <form>
-                            <div className='form-group mb-2'>
-                                <label className='form-label'>Todo title</label>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
+    const todo = { title, description, completed };
+    console.log(todo);
+
+    saveTodo(todo)
+      .then((response) => {
+        console.log(response.data);
+        navigate("/todos");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="container mt-4">
+      <div className="row">
+        <div className="card col-md-6 offset-md-3 offset-md-3">
+          <h2 className="text-center">Add Todo</h2>
+          <div className="card-body">
+            <form>
+              <div className="form-group mb-2">
+                <label className="form-label">Todo title</label>
+                <input
+                  className="form-control"
+                  placeholder="Enter todo title"
+                  name="title"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+              </div>
+              <div className="form-group mb-2">
+                <label className="form-label">Todo Description</label>
+                <input
+                  className="form-control"
+                  placeholder="Enter todo description"
+                  name="description"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                />
+              </div>
+              <div className="form-group mb-2">
+                <label className="form-label">Todo completed:</label>
+                <select
+                  className="form-control"
+                  value={completed}
+                  name="completed"
+                  onChange={(e) => setCompleted(e.target.value)}
+                >
+                  <option value="false">NO</option>
+                  <option value="true">YES</option>
+                </select>
+              </div>
+              <button
+                className="btn btn-success"
+                onClick={(e) => saveOrUpdateTodo(e)}
+              >
+                Save
+              </button>
+            </form>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default TodoComponent
+export default TodoComponent;
--- src/services/TodoService.js
+++ src/services/TodoService.js
@@ -2,4 +2,6 @@ import axios from "axios";
 
 const BASE_REST_API_URL = "http://localhost:8080/api/v1/todo";
 
-export const getAllTodos = () => axios.get(BASE_REST_API_URL + "/list")
+export const getAllTodos = () => axios.get(BASE_REST_API_URL + "/list");
+
+export const saveTodo = (todo) => axios.post(`${BASE_REST_API_URL}/add`, todo);
